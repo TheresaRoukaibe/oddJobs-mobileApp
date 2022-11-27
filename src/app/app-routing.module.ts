b467845ee8a28b6d36d5ff27e2c716660d44e3e7
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-11-27 19:27:44 +0200

    Implementing splashscreen routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+ },
+  {
+    path: 'splashscreen',
+    loadChildren: () => import('./splashscreen/splashscreen.module').then( m => m.SplashscreenPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/splashscreen/splashscreen.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-splashscreen',
+  templateUrl: './splashscreen.page.html',
+  styleUrls: ['./splashscreen.page.scss'],
+})
+export class SplashscreenPage implements OnInit {
+
+  constructor(public router:Router) { 
+    setTimeout(()=>{
+    this.router.navigateByUrl('');
+  }, 3000);
+  
+  }
+
+  ngOnInit() {
+  }
+
+}

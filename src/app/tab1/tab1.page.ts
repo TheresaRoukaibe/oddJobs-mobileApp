a78717e0f4237f8ce08055102609503512509102
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-11-29 20:23:23 +0200

    Routing to add job page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,8 +7,12 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {
+  constructor(private router:Router) {
     
+  }
+
+   goToAddPage() {
+    this.router.navigateByUrl('add-job');
   }
 
 }

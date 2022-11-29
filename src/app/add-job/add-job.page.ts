46091d248629516389effad77f0fe9fed470541b
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-11-29 21:35:47 +0200

    Finishing add job page design

--- src/app/add-job/add-job.page.ts
+++ src/app/add-job/add-job.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-job',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddJobPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  goBack(){
+    this.router.navigateByUrl('');
   }
 
 }

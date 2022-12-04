255abfb935a16e38dc1cc58931a5d7b241933922
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-12-04 12:44:58 +0200

    Starting save job backend

--- BackEnd/save_job.php
+++ BackEnd/save_job.php
@@ -9,8 +9,6 @@ $job_id = $_POST['job_id'];
 $user_id = $_POST['user_id'];
 $response = [];
 
-
-
 if((isset($_POST['job_id']) && !empty($job_id)) || (isset($_POST['user_id']) && !empty($user_id))){
    
     $query = $mysqli->prepare("SELECT * FROM jobs WHERE id=?");
@@ -23,16 +21,16 @@ if((isset($_POST['job_id']) && !empty($job_id)) || (isset($_POST['user_id']) && 
     $db_user_id = $job['user_id'];
 
     if($user_id == $db_user_id){
-            $response['status'] = "Applying to own job";
+            $response['status'] = "Favorite own job";
     }else{
     
     if($rows ==0){
         $response['status'] = "No job with this id";
     }else{
-       $apply_query = $mysqli->prepare("INSERT into applies_to(user_id, job_id) VALUES(?,?)");
+       $apply_query = $mysqli->prepare("INSERT into saves(user_id, job_id) VALUES(?,?)");
        $apply_query->bind_param("ii", $user_id, $job_id);
        $apply_query->execute();
-       $response['status'] = "User Applied for Job";
+       $response['status'] = "User Saved Job";
     }
     }
 
